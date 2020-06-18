fixed some data leak

numpy_nn/layers/Neuron.py
@@ -8,9 +8,10 @@ class Neuron(Layer):
     '''
     A Basic layer of neurons which fully connects to the next layer.
     '''
-    def __init__(self, num_inputs, num_outputs):
+    def __init__(self, num_inputs, num_outputs, name='Neuron_Layer'):
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
+        self.name = name
 
         self.weights = np.random.standard_normal((num_inputs, num_outputs))
         self.biases = np.random.standard_normal(num_outputs)
@@ -48,3 +49,4 @@ class Neuron(Layer):
     def optimize(self):
         self.weights = self.optimizer(self.weights, self.dL_dw, "weights")
         self.biases = self.optimizer(self.biases, self.dL_db, "biases")
+        

numpy_nn/optimizers/RMSProp.py
@@ -19,4 +19,5 @@ class RMSProp():
         beta = self.beta
         new_value = beta * value + (1-beta)*(dL**2)
         self.run_values[param_name] = new_value
-        return param - self.lr * dL/(np.sqrt(new_value) + self.epsilon)
+        result = param - self.lr * dL/(np.sqrt(new_value) + self.epsilon)
+        return result